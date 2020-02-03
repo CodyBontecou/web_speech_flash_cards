d3aecb3194b4e2b088f60748465a65f4fe5b9eb6
Cody Bontecou <bontecouc@gmail.com>
2020-02-03 10:13:02 -1000

    Going to set up test suite before making type changes. Typescript compilation is breaking my Javascript files.

--- public/scripts/speak.js
+++ public/scripts/speak.js
@@ -1,24 +1,24 @@
 var synth = window.speechSynthesis;
 var voiceSelect = document.querySelector('select');
 var voices = [];
-
 function populateVoiceList() {
     voices = synth.getVoices().sort(function (a, b) {
-        const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
-        if ( aname < bname ) return -1;
-        else if ( aname == bname ) return 0;
-        else return +1;
+        var aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+        if (aname < bname)
+            return -1;
+        else if (aname == bname)
+            return 0;
+        else
+            return +1;
     });
     var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
     voiceSelect.innerHTML = '';
-    for(i = 0; i < voices.length ; i++) {
+    for (i = 0; i < voices.length; i++) {
         var option = document.createElement('option');
         option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-
-        if(voices[i].default) {
+        if (voices[i]["default"]) {
             option.textContent += ' -- DEFAULT';
         }
-
         option.setAttribute('data-lang', voices[i].lang);
         option.setAttribute('data-name', voices[i].name);
         voiceSelect.appendChild(option);
@@ -26,20 +26,14 @@ function populateVoiceList() {
     voiceSelect.selectedIndex = selectedIndex;
 }
 populateVoiceList();
-
 if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
 }
-
 function speak(word, recognition) {
     var utterThis = new SpeechSynthesisUtterance(word);
     utterThis.onend = function (event) {
-        console.log('SpeechSynthesisUtterance.onend');
         recognition.start(); // I don't think recognition and speak should be intertwined like this.
-    }
-    utterThis.onerror = function (event) {
-        console.error('SpeechSynthesisUtterance.onerror');
-    }
+    };
     var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
     for (i = 0; i < voices.length; i++) {
         if (voices[i].name === selectedOption) {
